8f5636b7e3c61803965a8db94d397f7a146f545e
Serhiy Latyuk <serhiy_latyuk@inbox.ru>
2017-06-07 11:17:07 +0300

    fix uncorrect main.py after merging

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from controller import Controler
 from configparser import ConfigParser
 from postgresql_controller import PostgreSQLController
 from sqlite_controller import SQLiteController
+from mysql_controller import MysqlController
 
 if __name__ == "__main__":
     config = ConfigParser()
@@ -16,5 +17,7 @@ if __name__ == "__main__":
         controller = SQLiteController(Model())
     elif db_type == "postgres":
         controller = PostgreSQLController()
+    elif db_type == "mysql":
+        controller = MysqlController()
 
     controller.run()
